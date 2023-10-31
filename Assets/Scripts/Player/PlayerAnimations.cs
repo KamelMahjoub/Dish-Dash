8a2996f7983956086b4dd379570c331c32eb7093
KamelMahjoub <kamel.mahjoub@outlook.com>
2023-10-31 14:56:40 +0100

    Implemented Run Animation

---
+++ Assets/Scripts/Player/PlayerAnimations.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimations : MonoBehaviour
+{
+    private const string IsWalking = "IsWalking";
+    
+    private Animator animator;
+    [SerializeField] private Player player;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        animator.SetBool(IsWalking , player.IsWalking);
+    }
+}
