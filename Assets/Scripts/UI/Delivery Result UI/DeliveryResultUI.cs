e3ab9b621f470c30f2bd50925f0cd82b19e9ff7d
KamelMahjoub <kamel.mahjoub@outlook.com>
2023-11-08 02:43:17 +0100

    Assets Cleanup

--- Assets/Scripts/UI/Delivery Result UI/DeliveryResultUI.cs
+++ Assets/Scripts/UI/Delivery Result UI/DeliveryResultUI.cs
@@ -1,18 +1,50 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DeliveryResultUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private const string POPUP = "Popup";
+    [SerializeField] private Image backgroundImage;
+    [SerializeField] private Image iconImage;
+    [SerializeField] private TextMeshProUGUI messageText;
+    [SerializeField] private Color successColor;
+    [SerializeField] private Color failedColor;
+    [SerializeField] private Sprite successIcon;
+    [SerializeField] private Sprite failedIcon;
+    private Animator animator;
+
+    private void Awake()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
+        DeliveryManager.Instance.OnRecipeSuccess += DeliveryManager_OnRecipeSuccess;
+        DeliveryManager.Instance.OnRecipeFailed += DeliveryManager_OnRecipeFailed;
         
+        gameObject.SetActive(false);
+    }
+
+    private void DeliveryManager_OnRecipeFailed(object sender, EventArgs e)
+    {
+        gameObject.SetActive(true);
+        animator.SetTrigger(POPUP);
+        backgroundImage.color = failedColor;
+        iconImage.sprite = failedIcon;
+        messageText.text = "DELIVERY\nFAILED";
+    }
+
+    private void DeliveryManager_OnRecipeSuccess(object sender, EventArgs e)
+    {
+        gameObject.SetActive(true);
+        animator.SetTrigger(POPUP);
+        backgroundImage.color = successColor;
+        iconImage.sprite = successIcon;
+        messageText.text = "DELIVERY\nSUCCESS";
     }
 }
