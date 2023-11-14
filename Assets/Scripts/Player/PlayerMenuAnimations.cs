dde66c68199de18d5330a92f65ec92e12317ed83
KamelMahjoub <kamel.mahjoub@outlook.com>
2023-11-15 00:53:37 +0100

    Menu Setup

---
+++ Assets/Scripts/Player/PlayerMenuAnimations.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMenuAnimations : MonoBehaviour
+{
+    
+
+
+    private Animator animator;
+
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+   
+}
